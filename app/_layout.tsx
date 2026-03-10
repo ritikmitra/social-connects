08e456612a594a412262ef04d811719f9eb7109e
Ritik Mitra <ritikmitra36@gmail.com>
2026-03-11 01:08:03 +0530

    feat: enhance notification handling with channel setup and alert configuration
    - Integrated Expo Notifications for handling alerts and sound settings.
    - Added a notification channel for Android devices to manage notification behavior.
    - Updated the layout component to include notification setup logic.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,36 @@
+import * as Notifications from "expo-notifications";
 import { ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProviderCustom, useAppTheme } from '@/context/ThemeContext';
 import SafeScreen from '@/components/SafeScreen';
 import { SocketProvider } from "@/context/socket.context";
+import { useEffect } from "react";
+import { Platform } from "react-native";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
+
 function RootNavigator() {
   const { theme } = useAppTheme();
+
+  useEffect(() => {
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+  }, []);
 
   return (
     <ThemeProvider value={theme}>
@@ -16,7 +41,7 @@ function RootNavigator() {
           <Stack.Screen name="auth/email" />
           <Stack.Screen name="auth/login" />
           <Stack.Screen name="auth/register" />
-          <Stack.Screen name="(tabs)"/>
+          <Stack.Screen name="(tabs)" />
         </Stack>
         <StatusBar style={theme.dark ? 'light' : 'dark'} />
       </SafeScreen>
